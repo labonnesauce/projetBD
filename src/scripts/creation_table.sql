9541d918fc3010485117fcd357a925c0db636f83
olivier Lajoie <olivier.lajoie.2@ulaval.ca>
2022-03-17 11:13:21 -0400

    finalisation diagramme

--- src/scripts/creation_table.sql
+++ src/scripts/creation_table.sql
@@ -7,31 +7,31 @@ DROP TABLE IF EXISTS Livreur;
 DROP TABLE IF EXISTS Commander;
 
 CREATE TABLE IF NOT EXISTS Client (
-    id        INTEGER AUTO_INCREMENT PRIMARY KEY,
-    telephone VARCHAR(10)   NOT NULL,
+    id        INT(20) AUTO_INCREMENT PRIMARY KEY,
+    telephone VARCHAR(20)   NOT NULL,
     courriel  VARCHAR(100)  NOT NULL UNIQUE,
     adresse   VARCHAR(100)  NOT NULL,
     nom       VARCHAR(100)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS MotDePasse (
-    id           INTEGER      PRIMARY KEY,
+    client_id           INT(20)     PRIMARY KEY,
     mot_de_passe VARCHAR(100) NOT NULL,
-      CONSTRAINT FK_client_id_motpasse FOREIGN KEY (id) REFERENCES Client (id)
+      CONSTRAINT FK_client_id_motpasse FOREIGN KEY (client_id) REFERENCES Client (id)
 );
 
 CREATE TABLE IF NOT EXISTS Categorie (
-    id          INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id          INT(20)  AUTO_INCREMENT PRIMARY KEY,
     nom         VARCHAR(100),
     description VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS Produit (
-    id           INTEGER      PRIMARY KEY,
-    categorie_id INTEGER,
+    id           INT(20)      PRIMARY KEY,
+    categorie_id INT(20) ,
     nom          VARCHAR(100) NOT NULL UNIQUE,
-    prix         INTEGER      NOT NULL,
-    poids        INTEGER      NOT NULL,
+    prix         FLOAT(10,2)       NOT NULL,
+    poids        INT(20)       NOT NULL,
     description  VARCHAR(500) NOT NULL,
       CONSTRAINT FK_categorie_id FOREIGN KEY (categorie_id) REFERENCES Categorie (id)
 );
@@ -45,17 +45,17 @@ CREATE TABLE IF NOT EXISTS Souhaiter (
 );
 
 CREATE TABLE IF NOT EXISTS Livreur (
-      id     INTEGER PRIMARY KEY,
+      id     INT(20)  PRIMARY KEY,
       statut ENUM('occupe', 'attente'),
       nom    VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Commander (
-      id             INTEGER PRIMARY KEY AUTO
-      produit_id     INTEGER NOT NULL,
-      client_id      INTEGER NOT NULL,
-      livreur_id     INTEGER NOT NULL,
-      prix           INTEGER NOT NULL,
+      id             INT(20)  PRIMARY KEY AUTO
+      produit_id     INT(20)  NOT NULL,
+      client_id      INT(20)  NOT NULL,
+      livreur_id     INT(20)  NOT NULL,
+      prix           FLOAT(10,2)  NOT NULL,
       date_commande  DATE NOT NULL,
       date_livraison DATE NOT NULL,
         PRIMARY KEY (id, produit_id, client_id),
