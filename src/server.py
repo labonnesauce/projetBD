d0452a45b16877b6fc01b34f776db1656a0d0b40
JoBess7 <JOBES10@ulaval.ca>
2022-03-17 21:20:48 -0400

    ajout des folders statiques

--- src/server.py
+++ src/server.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, jsonify, request, Response
+from util import connection as bd
+from util import insertion_tables as insertion
 
 app = Flask(__name__)
-
+model = {}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/add-todo/", methods=["POST"])
-def add_todo():
-    x = 1
-
-
-@app.route("/todos/", methods=["GET"])
-def get_todos():
-    x = 1
+    cursor.execute("SELECT * FROM Souhaiter LIMIT 1")
+    return render_template("index.html", x=cursor.fetchone())
 
 
 if __name__ == '__main__':
+    global conn
+    global cursor
+
+    # Rouler la commande "mysql -u root -p < scripts/creation_bd.sql" avant de lancer ce fichier.
+    bd.execute_file("scripts/creation_table.sql")
+    insertion.insert_donnees()
+
+    conn, cursor = bd.open_connection()
     app.run()
