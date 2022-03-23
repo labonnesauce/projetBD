69cd9442db654969c3877f5bf9d1916d96ad2363
olivier Lajoie <olivier.lajoie.2@ulaval.ca>
2022-03-23 13:47:08 -0400

    ajout quelques procédures

--- src/scripts/creation_routines.sql
+++ src/scripts/creation_routines.sql
@@ -1,14 +1,26 @@
 DELIMITER //
-CREATE FUNCTION
-  VisibiliteProduitFiltreParNom(idProduit INT(20), nomProduit VARCHAR(100), filtre VARCHAR(100)) RETURNS INTEGER;
-  BEGIN
-    DECLARE visible INTEGER;
+CREATE TRIGGER HeureLivraisonInvalide
+    BEFORE INSERT ON commander
+    FOR EACH ROW
+    BEGIN
+        IF (SELECT commander.date_livraison FROM commander )< (SELECT commander.date_commande FROM commander)
+        THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = "Date de livraison invalide";
+        end if;
+    end //
+DELIMITER ;
 
 
-    IF stock - NEW.quantite < 0
-    THEN SET NEW.id_acheteur = NULL;
-    ELSE
-      UPDATE Vendre V SET V.quantite = V.quantite - NEW.quantite WHERE V.id_biere = NEW.id_biere;
-    END IF;
-  END//
-DELIMITER;
+DELIMITER //
+CREATE TRIGGER AssignationLivreur
+    BEFORE UPDATE ON commander
+    FOR EACH ROW
+    BEGIN
+        if (SELECT COUNT(*) FROM livreur L WHERE L.statut = 'attente') = 0
+        THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = "Aucun livreur n'est disponible en ce moment";
+        end if;
+    end //
+DELIMITER ;
