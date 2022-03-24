110a84e941a681426debd71fb23f3cd8febd6877
olivier Lajoie <olivier.lajoie.2@ulaval.ca>
2022-03-24 14:33:22 -0400

    petits changements prix total

--- src/scripts/creation_routines.sql
+++ src/scripts/creation_routines.sql
@@ -1,4 +1,15 @@
-#fonction total commande, je recois id_produit et quantité. prix fix taxes
+DELIMITER //
+CREATE FUNCTION PrixTotal(produit_id INT(20), quantite INT(20)) RETURNS FLOAT(10,2)
+BEGIN
+        DECLARE item FLOAT(10,2);
+        DECLARE total FLOAT(10,2);
+
+        SELECT P.prix into item FROM Produit P, Commander C WHERE C.id = produit_id;
+        SET total = item * quantite * 1.15 + 10;
+        RETURN total;
+end //
+DELIMITER ;
+
 
 DELIMITER //
 CREATE TRIGGER HeureLivraisonInvalide
