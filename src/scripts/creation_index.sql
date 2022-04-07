ff828a64267ef02632c78ff9df16ebbc38a02bd0
olivier Lajoie <olivier.lajoie.2@ulaval.ca>
2022-04-07 10:50:51 -0400

    enelver quelques indexes

--- src/scripts/creation_index.sql
+++ src/scripts/creation_index.sql
@@ -2,15 +2,11 @@ use projet_glo2005;
 
 CREATE INDEX indexClient ON client(id) USING BTREE;
 
-CREATE INDEX indexMotDePasse ON MotDePasse(id, mot_de_passe) USING BTREE;
-
 CREATE INDEX indexCategorie ON Categorie(id, nom) USING BTREE;
 
 CREATE INDEX indexProduit ON Produit(id, categorie_id) USING BTREE;
 
 CREATE INDEX indexSouhaiter ON Souhaiter(client_id, produit_id) USING BTREE;
-
-CREATE INDEX indexLivreur ON Livreur(id, statut) USING BTREE;
 
 CREATE INDEX indexCommander ON Commander(id, produit_id, client_id, livreur_id, prix, date_commande, date_livraison,
                                          quantite) USING BTREE;
