60299a10500f3745f3826e34ca2352006cbc5fe8
JoBess7 <JOBES10@ulaval.ca>
2022-04-13 21:25:22 -0400

    finitions

--- src/scripts/creation_index.sql
+++ src/scripts/creation_index.sql
@@ -1,12 +1,3 @@
 use projet_glo2005;
 
-CREATE INDEX indexCategorie ON Categorie(id, nom) USING BTREE;
-
-CREATE INDEX indexProduit ON Produit(id, categorie_id) USING BTREE;
-
-CREATE INDEX indexSouhaiter ON Souhaiter(client_id, produit_id) USING BTREE;
-
-CREATE INDEX indexCommander ON Commander(id, produit_id, client_id, livreur_id, prix, date_commande, date_livraison,
-                                         quantite) USING BTREE;
-
 CREATE INDEX indexStatutLivreurs ON Livreur(statut) USING BTREE;
